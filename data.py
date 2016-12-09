706ef06808fb5cbb4b0ef414454081bc67023a85
chengzhu <cyu0913@gmail.com>
2016-12-09 16:11:13 -0600

    wav to english text is trainable.

--- data.py
+++ data.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 import sugartensor as tf
 import numpy as np
-
+import pandas as pd
+import librosa
+import glob
+import os
+import string
+import itertools
+import pdb
 
 __author__ = 'buriburisuri@gmail.com'
 
@@ -98,3 +104,115 @@ class ComTrans(object):
                 elif ch == 1:  # <EOS>
                     break
             print '[%d]' % i + str_
+
+
+class TedTrans(object):
+
+    def __init__(self, batch_size=16, fs=44100, data_path='asset/ted/', name='train'):
+
+        @tf.sg_producer_func
+        def _load_mfcc(src_list):
+            lab, wav = src_list  # label, wave_file
+            # load wave file
+            wav, sr = librosa.load(wav, mono=True)
+            # mfcc
+            mfcc = librosa.feature.mfcc(wav, sr)
+            # return result
+            return lab, mfcc
+
+        self.fs = fs;     
+
+        # load corpus
+        targets, wave_files = self._load_corpus(data_path)
+
+        # to constant tensor
+        target = tf.convert_to_tensor(targets)
+        wave_file = tf.convert_to_tensor(wave_files)
+
+        # create queue from constant tensor
+        target, wave_file = tf.train.slice_input_producer([target, wave_file], shuffle=True)
+
+        # decode wave file
+        target, mfcc = _load_mfcc(source=[target, wave_file], dtypes=[tf.sg_intx, tf.sg_floatx],
+                                 capacity=128, num_threads=32)
+
+        # create batch queue with dynamic pad
+        batch_queue = tf.train.batch([target, mfcc], batch_size,
+                                     shapes=[(self.max_len,), (20, None)],
+                                     num_threads=32, capacity=batch_size*64,
+                                     dynamic_pad=True)
+
+        # split data
+        self.target,  self.mfcc = batch_queue
+        # batch * time * dim
+        self.mfcc = self.mfcc.sg_transpose(perm=[0, 2, 1])
+
+        # calc total batch count
+        self.num_batch = len(targets) // batch_size
+
+        # print info
+        tf.sg_info('TED EN-CN corpus loaded.(total data=%d, total batch=%d)' % (len(targets), self.num_batch))
+
+    def _load_corpus(self, data_path):
+
+        # make file ID
+        file_ids = []
+        for d in [data_path + 'text.en/']:
+            file_ids.extend([f[18:-4] for f in sorted(glob.glob(d + '*.txt'))])
+
+        # make wave file list
+        wav_files = [data_path + 'wav.en/' + f + '.wav' for f in file_ids]
+
+        # exclude extremely short wave files
+        file_id, wav_file = [], []
+        self.min_duration = 0;
+        self.max_duration = 10; # to be optimally determined based on our data, and pooling size later. CY
+        for i, w in zip(file_ids, wav_files):
+            if os.stat(w).st_size > self.fs * self.min_duration and os.stat(w).st_size < self.fs * self.max_duration: 
+                file_id.append(i)
+                wav_file.append(w)
+
+        all_byte, targets = [], []
+        for f in file_id:
+            # remove punctuation, to lower
+            s = ' '.join(open(data_path + 'text.en/' + f + '.txt').read()
+                         .translate(None, string.punctuation).lower().split())
+            tgt = [ord(ch) for ch in s]
+            targets.append(tgt)
+            all_byte.extend(tgt)
+
+        self.index2byte = [0, 1] + list(np.unique(all_byte))  # add <EMP>, <EOS> tokens  
+        self.byte2index = {}
+
+        for i, b in enumerate(self.index2byte):
+            self.byte2index[b] = i 
+        self.voca_size = len(self.index2byte)
+        self.min_len =  0
+        self.max_len =  100
+
+        tgt, wav = [], []
+        for t, w in zip(targets, wav_files):
+            if len(t) > self.min_len and len(t) < self.max_len:
+                tgt.append(t)
+                wav.append(w)
+
+        # convert to index list and add <EOS> to end of sentence
+        for i in range(len(tgt)):
+            tgt[i] = [self.byte2index[ch] for ch in tgt[i]] + [1] 
+
+        # zero-padding
+        for i in range(len(tgt)):
+            tgt[i] += [0] * (self.max_len - len(tgt[i]))
+
+        return tgt, wav
+        
+    def print_index(self, indices):
+        # transform label index to character
+        for i, index in enumerate(indices):
+            str_ = ''
+            for ch in index:
+                if ch > 0:
+                    str_ += unichr(self.index2byte[ch])
+                elif ch == 0:  # <EOS>
+                    break
+            print str_
--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import sugartensor as tf
+from data import TedTrans
 from data import ComTrans
-
+import pdb
 
 __author__ = 'buriburisuri@gmail.com'
 
@@ -16,6 +17,7 @@ tf.sg_verbosity(10)
 
 batch_size = 16    # batch size
 latent_dim = 400   # hidden layer dimension
+num_dim = 128      # latent dimension
 num_blocks = 3     # dilated blocks
 
 #
@@ -23,19 +25,17 @@ num_blocks = 3     # dilated blocks
 #
 
 # ComTrans parallel corpus input tensor ( with QueueRunner )
-data = ComTrans(batch_size=batch_size)
+data = TedTrans(batch_size=batch_size)
 
 # source, target sentence
-x, y = data.source, data.target
+x, y = data.mfcc, data.target
 voca_size = data.voca_size
 
-# make embedding matrix for source and target
-emb_x = tf.sg_emb(name='emb_x', voca_size=voca_size, dim=latent_dim)
+# make embedding matrix for target
 emb_y = tf.sg_emb(name='emb_y', voca_size=voca_size, dim=latent_dim)
 
 # shift target for training source
 y_src = tf.concat(1, [tf.zeros((batch_size, 1), tf.sg_intx), y[:, :-1]])
-
 
 # residual block
 @tf.sg_sugar_func
@@ -62,13 +62,10 @@ def sg_res_block(tensor, opt):
 # inject residual multiplicative block
 tf.sg_inject_func(sg_res_block)
 
+# expand dimension
+#enc = x.sg_conv1d(size=1, dim=num_dim, act='tanh', bn=True)
+enc = x;
 
-#
-# encode graph ( atrous convolution )
-#
-
-# embed table lookup
-enc = x.sg_lookup(emb=emb_x)
 # loop dilated conv block
 for i in range(num_blocks):
     enc = (enc
@@ -78,9 +75,18 @@ for i in range(num_blocks):
            .sg_res_block(size=5, rate=8)
            .sg_res_block(size=5, rate=16))
 
+# pooling before passed to decodering
+enc = tf.expand_dims(enc, 1);
+enc = tf.nn.max_pool(enc, [1, 1, 10, 1], [1, 1, 10, 1], 'VALID')
+enc = tf.squeeze(enc, [1])
+
+# zero padding encoder output to have the size equal to self.max_len
+enc = tf.transpose(enc, perm=[1, 2, 0])
+enc = tf.image.resize_image_with_crop_or_pad(enc, data.max_len, 20)
+enc = tf.transpose(enc, perm=[2, 0 ,1])
+
 # concat merge target source
 enc = enc.sg_concat(target=y_src.sg_lookup(emb=emb_y))
-
 
 #
 # decode graph ( causal convolution )
@@ -93,8 +99,7 @@ for i in range(num_blocks):
            .sg_res_block(size=3, rate=1, causal=True)
            .sg_res_block(size=3, rate=2, causal=True)
            .sg_res_block(size=3, rate=4, causal=True)
-           .sg_res_block(size=3, rate=8, causal=True)
-           .sg_res_block(size=3, rate=16, causal=True))
+           .sg_res_block(size=3, rate=8, causal=True))
 
 # final fully convolution layer for softmax
 dec = dec.sg_conv1d(size=1, dim=data.voca_size)
@@ -102,7 +107,8 @@ dec = dec.sg_conv1d(size=1, dim=data.voca_size)
 # cross entropy loss with logit and mask
 loss = dec.sg_ce(target=y, mask=True)
 
+
 # train
-tf.sg_train(log_interval=30, lr=0.0001, loss=loss,
+tf.sg_train(log_interval=30, lr=0.00001, loss=loss,
             ep_size=data.num_batch, max_ep=20, early_stop=False)
 
